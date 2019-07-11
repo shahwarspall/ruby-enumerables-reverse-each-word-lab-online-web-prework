b410c63904d6353daa96228ff7639e5fc9550aa9
Shahwar Spall <shevy.spall@gmail.com>
2019-07-11 05:00:55 +0000

    Done.

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -4,5 +4,14 @@ def reverse_each_word(string)
   string.each do |backward|
     array << backward.reverse
   end
-  memo
+  array.join(" ")
+end
+
+def reverse_each_word(string)
+  string= string.split
+  array= []
+  string.collect do |backward|
+    array << backward.reverse
+  end
+  array.join(" ")
 end
